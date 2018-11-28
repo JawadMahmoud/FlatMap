788f655fdf11368fb5317bd2ecab6e5c7624c9d0
Jawad Mahmoud <jd.mahmoud@gmail.com>
2018-11-28 04:14:12 +0000

    Finalised the Single page design and implementation, One-click initiation and toggle buttons for info retrieval

--- Scripts/main.js
+++ Scripts/main.js
@@ -3,6 +3,7 @@
 var url;
 var gpx;
 var file_name_ext, uploaded_file;
+var avgtemp;
 
 var plotcords;
 var elecords;
@@ -21,8 +22,18 @@ var ele_max;
 
 var marker_list;
 var markers_layer;
+var markersLayerG;
 
 var eleHotlineLayer, hrHotlineLayer, cadHotlineLayer, tempHotlineLayer;
+var eleHotlineLayerG, hrHotlineLayerG, cadHotlineLayerG, tempHotlineLayerG;
+var eleHotlineMarkers, hrHotlineMarkers, cadHotlineMarkers, tempHotlineMarkers;
+
+var eleicon, hricon, cadicon, tempicon;
+
+// var minhrpic, midhrpic, maxhrpic = "Icons/heart1.png"; "Icons/heart2.png"; "Icons/heart3.png";
+// var minelepic, midelepic, maxelepic = "Icons/hill1.png"; "Icons/hill2.png"; "Icons/hill3.png";
+// var mincadpic, midcadpic, maxcadpic = "Icons/cad1.png"; "Icons/cad2.png"; "Icons/cad3.png";
+// var mintemppic, midtemppic, maxtemppic = "Icons/temp1.png"; "Icons/temp2.png"; "Icons/temp3.png";
 
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiamF3YWRzaGFoaWQiLCJhIjoiY2puZzQ0eno3MDRjbDNrcWlsZjZxNTcxaSJ9.mavbvckNOMnntmxWcKboyQ', {
@@ -36,6 +47,9 @@ var control = L.control.layers(null, null).addTo(mymap);
 
 
 $(document).ready(function(){
+  $('#right_column').hide();
+  $('#left_column').hide();
+  $('#middle_column').hide();
   $('#get_file').tooltip('show'); 
   //document.getElementsByClassName("leaflet-control-layers-toggle").id = "MapControl";
   //document.getElementsByClassName("leaflet-control-layers-toggle").title = "Check here for Different Layers and Markers";
@@ -48,20 +62,49 @@ $('#get_file').click(function() {
   $('#get_file').tooltip('hide');
 });
 
-$('#chart_file').click(function() {
-  $('#chart_file').tooltip('hide');
+$('#hr_layer_button').click(function() {
+  $('#hr_layer_button').tooltip('hide');
 });
-$('#All-layers-button').click(function() {
-  $('#All-layers-button').tooltip('hide');
-  $('.leaflet-control-layers-toggle').tooltip('show'); 
+$('#ele_layer_button').click(function() {
+  $('#ele_layer_button').tooltip('hide');
 });
-$('#All-markers-button').click(function() {
-  $('#All-markers-button').tooltip('hide');
-  $('.leaflet-control-layers-toggle').tooltip('show'); 
+$('#temp_layer_button').click(function() {
+  $('#temp_layer_button').tooltip('hide');
 });
+$('#cad_layer_button').click(function() {
+  $('#cad_layer_button').tooltip('hide');
+});
+
+$('#hr_layer_button').hover(function() {
+  $('#hr_layer_button').tooltip('show');
+});
+$('#ele_layer_button').hover(function() {
+  $('#ele_layer_button').tooltip('show');
+});
+$('#temp_layer_button').hover(function() {
+  $('#temp_layer_button').tooltip('show');
+});
+$('#cad_layer_button').hover(function() {
+  $('#cad_layer_button').tooltip('show');
+});
+
 $('.leaflet-control-layers-toggle').hover(function() {
   $('.leaflet-control-layers-toggle').tooltip('hide');
 });
+$('.leaflet-control-layers-toggle').hide();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 document.getElementById('get_file').onclick = function() {
   document.getElementById('gpx_file').click();
@@ -73,8 +116,17 @@ document.getElementById('chart_file').onclick = function() {
   display_chart(timecords, hrdata, 'Heart Rate', 'myChart2', 'SlateBlue');
 };
 
+// $(document).on('charts_event', function() {
+
+//   $('All-layers-button').click();
+//   //$('#chart_file').click();
+// })
+
 $(document).ready(function(){
   $("#gpx_file").on('change', function(){
+    $('#right_column').show();
+    $('#left_column').show();
+    $('#middle_column').show();
     uploaded_file = document.getElementById('gpx_file').value;
     file_name_ext = uploaded_file.split('.').pop().toLowerCase();
     if ( 'gpx' !== file_name_ext ) {
@@ -84,11 +136,11 @@ $(document).ready(function(){
     console.log(file_name_ext);
 
     remove_layer(gpx);
-    remove_layer(markers_layer);
-    remove_layer(hrHotlineLayer);
-    remove_layer(eleHotlineLayer);
-    remove_layer(tempHotlineLayer);
-    remove_layer(cadHotlineLayer);
+    remove_layer(markersLayerG);
+    remove_layer(hrHotlineLayerG);
+    remove_layer(eleHotlineLayerG);
+    remove_layer(tempHotlineLayerG);
+    remove_layer(cadHotlineLayerG);
     display_gpx(document.getElementById('demo'));
     //$.when( display_gpx(document.getElementById('demo')) ).done( draw_hotline_all() );
   });
@@ -142,6 +194,7 @@ var new_gpx = new L.GPX(url, {
   _c('speed').textContent    = gpx.get_moving_speed().toFixed(2);
   _c('avghr').textContent    = gpx.get_average_hr();
   _c('avgcad').textContent   = gpx.get_average_cadence();
+  avgtemp = gpx.get_average_temp();
   //_c('avgtemp').textContent  = gpx.get_average_temp();
   //_c('elevation-gain').textContent = gpx.get_elevation_gain().toFixed(0);
   //_c('elevation-loss').textContent = gpx.get_elevation_loss().toFixed(0);
@@ -166,35 +219,52 @@ var new_gpx = new L.GPX(url, {
     //console.log(eledata);
 
   loadXMLDoc();
+  window.setTimeout(function(){draw_hotline_all();
+    display_chart(timecords, eledata, 'Elevation', 'myChart1', 'teal',  "card-to-left");
+    display_chart(timecords, hrdata, 'Heart Rate', 'myChart2', 'SlateBlue', "card-to-right");}, 200);
 
 }).addTo(mymap);
 
-document.getElementById("layers-buttons-group").style.display = "block";
-$('#chart_file').tooltip('show');
-$('#All-layers-button').tooltip('show'); 
-$('#All-markers-button').tooltip('show'); 
+//document.getElementById("layers-buttons-group").style.display = "block";
+//$('#chart_file').tooltip('show');
+//$('#All-layers-button').tooltip('show'); 
+//$('#All-markers-button').tooltip('show'); 
 $('#file_success_alert').show();
 document.getElementById("myChart1").style.display = "none";
 document.getElementById("myChart2").style.display = "none";
 
+//$(document).trigger('charts_event');
+
+
 }
 
 function draw_hotline_all() {
+  all_marker_groups();
   remove_layer(hrHotlineLayer);
   remove_layer(eleHotlineLayer);
   remove_layer(tempHotlineLayer);
   remove_layer(cadHotlineLayer);
   if (hrcords.length != 0) {
     draw_hotline_hr();
+    $('#hr_layer_button').show();
   }
   if (elecords.length != 0) {
     draw_hotline_ele();
+    $('#ele_layer_button').show();
   }
   if (tempcords.length != 0) {
     draw_hotline_temp();
+    $('#temp_layer_button').show();
+    document.getElementById('cad_header').innerText = "Average Temperature";
+    console.log("temp:", gpx.get_average_temp());
+    //document.getElementsByClassName('avgcad').textContent = gpx.get_average_temp();
+    document.getElementById("avg_cad").innerText = avgtemp;
+    document.getElementById('cad_unit').innerText = "°C";
+    document.getElementById('cad_img').src = "https://image.flaticon.com/icons/svg/134/134125.svg";
   }
   if (cadcords.length != 0) {
     draw_hotline_cad();
+    $('#cad_layer_button').show();
   }
   
 }
@@ -208,12 +278,14 @@ eleHotlineLayer = L.hotline(elecords, {
             0.5: '#fbff12',
             1.0: '#ff206e'
         },
-        weight: 3,
+        weight: 7,
         outlineColor: '#000000',
         outlineWidth: 1
 })
 //control.removeLayer(eleHotlineLayer).addTo(mymap);
-control.addBaseLayer(eleHotlineLayer, 'Elevation Line').addTo(mymap);
+eleHotlineLayerG.push(eleHotlineLayer);
+eleHotlineLayerG = L.layerGroup(eleHotlineLayerG);
+control.addBaseLayer(eleHotlineLayerG, 'Elevation Line').addTo(mymap);
 }
 
 function draw_hotline_hr() {
@@ -225,12 +297,14 @@ hrHotlineLayer = L.hotline(hrcords, {
             0.5: '#fbff12',
             1.0: '#ff206e'
         },
-        weight: 3,
+        weight: 7,
         outlineColor: '#000000',
         outlineWidth: 1
 })
 //control.removeLayer(hrHotlineLayer).addTo(mymap);
-control.addBaseLayer(hrHotlineLayer, 'HeartRate Line').addTo(mymap);
+hrHotlineLayerG.push(hrHotlineLayer);
+hrHotlineLayerG = L.layerGroup(hrHotlineLayerG);
+control.addBaseLayer(hrHotlineLayerG, 'Heart Rate Line').addTo(mymap);
 }
 
 function draw_hotline_temp() {
@@ -242,12 +316,14 @@ tempHotlineLayer = L.hotline(tempcords, {
             0.5: '#fbff12',
             1.0: '#ff206e'
         },
-        weight: 3,
+        weight: 7,
         outlineColor: '#000000',
         outlineWidth: 1
 })
 //control.removeLayer(tempHotlineLayer).addTo(mymap);
-control.addBaseLayer(tempHotlineLayer, 'Temperature Line').addTo(mymap);
+tempHotlineLayerG.push(tempHotlineLayer);
+tempHotlineLayerG = L.layerGroup(tempHotlineLayerG);
+control.addBaseLayer(tempHotlineLayerG, 'Temperature Line').addTo(mymap);
 }
 
 function draw_hotline_cad() {
@@ -259,12 +335,14 @@ cadHotlineLayer = L.hotline(cadcords, {
             0.5: '#fbff12',
             1.0: '#ff206e'
         },
-        weight: 3,
+        weight: 7,
         outlineColor: '#000000',
         outlineWidth: 1
 })
 //control.removeLayer(cadHotlineLayer).addTo(mymap);
-control.addBaseLayer(cadHotlineLayer, 'Cadence Line').addTo(mymap);
+cadHotlineLayerG.push(cadHotlineLayer);
+cadHotlineLayerG = L.layerGroup(cadHotlineLayerG);
+control.addBaseLayer(cadHotlineLayerG, 'Cadence Line').addTo(mymap);
 }
 
 function loadXMLDoc() {
@@ -374,11 +452,12 @@ function remove_layer(layer) {
 }
 
 
-function display_chart(inlabels, indata, inchartlabel, inelement, incolor) {
+function display_chart(inlabels, indata, inchartlabel, inelement, incolor, incard) {
   console.log(indata);
   console.log(inlabels);
   document.getElementById(inelement).style.display = "block";
   document.getElementById(inelement).innerHTML = "";
+  document.getElementById(incard).style.marginTop = "4%"
   var ctx = document.getElementById(inelement).getContext('2d');
   new Chart(ctx, {
     type: 'line',
@@ -406,6 +485,195 @@ function display_chart(inlabels, indata, inchartlabel, inelement, incolor) {
     }
 });
 }
+
+
+function all_marker_groups() {
+  if (elecords.length != 0) {eleHotlineLayerG = []}
+  if (hrcords.length != 0) {hrHotlineLayerG = []}
+  if (cadcords.length != 0) {cadHotlineLayerG = []}
+  if (tempcords.length != 0) {tempHotlineLayerG = []}
+  markersLayerG = [];
+
+  for (j = 0; j < plotcords.length; j += 10) {
+    var invisiblemarker = L.marker(plotcords[j], { riseOnHover : 'true', icon : L.icon({ iconUrl : 'Icons/blank2.png', iconSize : [5,5], iconAnchor : [2.5,5]})});
+    invisiblemarker.bindTooltip(
+      "<b>Elevation: </b>" + eledata[j].toFixed(2) + "<br />" +
+      "<b>Heart Rate: </b>" + hrdata[j].toFixed(2) + "<br />" +
+      "<b>Temperature: </b>" + tempdata[j] + "<br />" +
+      "<b>Cadence: </b>" + caddata[j] + "<br />"
+    );
+    markersLayerG.push(invisiblemarker);
+    if (j % 100 == 0) {
+      if (elecords.length != 0) {
+        if (eledata[j] < 50) {
+          eleicon = 'Icons/hill1.png';
+        } else {
+          if (eledata[j] < 100) {
+            eleicon = 'Icons/hill2.png';
+          } else {
+            eleicon = 'Icons/hill3.png';
+          }
+        }
+        var elemarker = L.marker(plotcords[j], { riseOnHover : 'true', icon : L.icon({ iconUrl : eleicon, iconSize : [20,16.5], iconAnchor : [10,16.5]})});
+        elemarker.bindPopup("<b>Elevation: </b>" + eledata[j].toFixed(2));
+        eleHotlineLayerG.push(elemarker);
+      }
+      if (hrcords.length != 0) {
+        if (hrdata[j] < 100) {
+          hricon = 'Icons/heart1.png';
+        } else {
+          if (hrdata[j] < 150) {
+            hricon = 'Icons/heart2.png';
+          } else {
+            hricon = 'Icons/heart3.png';
+          }
+        }
+        var hrmarker = L.marker(plotcords[j], { riseOnHover : 'true', icon : L.icon({ iconUrl : hricon, iconSize : [20,19], iconAnchor : [10,19]})});
+        hrmarker.bindPopup("<b>Heart Rate: </b>" + hrdata[j].toFixed(2));
+        hrHotlineLayerG.push(hrmarker);
+      }
+      if (cadcords.length != 0) {
+        if (caddata[j] < 50) {
+          cadicon = 'Icons/cad1.png';
+        } else {
+          if (caddata[j] < 100) {
+            cadicon = 'Icons/cad2.png';
+          } else {
+            cadicon = 'Icons/cad3.png';
+          }
+        }
+        var cadmarker = L.marker(plotcords[j], { riseOnHover : 'true', icon : L.icon({ iconUrl : cadicon, iconSize : [19,18], iconAnchor : [9.5,18]})});
+        cadmarker.bindPopup("<b>Cadence: </b>" + caddata[j].toFixed(2));
+        cadHotlineLayerG.push(cadmarker);
+      }
+      if (tempcords.length != 0) {
+        if (tempdata[j] < 20) {
+          tempicon = 'Icons/temp1.png';
+        } else {
+          if (tempdata[j] < 35) {
+            tempicon = 'Icons/temp2.png';
+          } else {
+            tempicon = 'Icons/temp3.png';
+          }
+        }
+        var tempmarker = L.marker(plotcords[j], { riseOnHover : 'true', icon : L.icon({ iconUrl : tempicon, iconSize : [10,20], iconAnchor : [5,20]})});
+        tempmarker.bindPopup("<b>Temperature: </b>" + tempdata[j]);
+        tempHotlineLayerG.push(tempmarker);
+      }
+    }
+    
+  }
+  markersLayerG = L.layerGroup(markersLayerG);
+  control.addOverlay(markersLayerG, 'Markers').addTo(mymap);
+
+}
+
+$('#hr_layer_button').hide();
+$('#ele_layer_button').hide();
+$('#cad_layer_button').hide();
+$('#temp_layer_button').hide();
+
+
+
+
+
+$("#hr_layer_button").click(function() {
+  //event.preventDefault();
+  if(mymap.hasLayer(hrHotlineLayerG)) {
+      //$(this).removeClass('selected');
+      $("#hr_layer_button").attr('class', 'btn btn-outline-danger');
+      //$("#hr_layer_button").attr('title', "<p>Click to toggle gradient line for <b>Heart Rate</b> values <u>ON</u><br><em>RED:</em> High<br><em>GOLD:</em> Medium<br><em>GREEN:</em> Low");
+      mymap.removeLayer(hrHotlineLayerG);
+      mymap.addLayer(gpx);
+      mymap.addLayer(markersLayerG);
+  } else {
+      mymap.addLayer(hrHotlineLayerG); 
+      mymap.addLayer(markersLayerG);
+      if(mymap.hasLayer(gpx)){mymap.removeLayer(gpx);}
+      if(mymap.hasLayer(eleHotlineLayerG)){mymap.removeLayer(eleHotlineLayerG);}
+      if(mymap.hasLayer(cadHotlineLayerG)){mymap.removeLayer(cadHotlineLayerG);}
+      if(mymap.hasLayer(tempHotlineLayerG)){mymap.removeLayer(tempHotlineLayerG);}
+
+      //$(this).addClass('selected');
+      //$("#hr_layer_button").attr('title', "<p>Click to toggle gradient line for <b>Heart Rate</b> values <u>OFF</u><br><em>RED:</em> High<br><em>GOLD:</em> Medium<br><em>GREEN:</em> Low");
+      $("#hr_layer_button").attr('class', 'btn btn-danger');
+      $("#ele_layer_button").attr('class', 'btn btn-outline-warning');
+      $("#temp_layer_button").attr('class', 'btn btn-outline-info');
+      $("#cad_layer_button").attr('class', 'btn btn-outline-success');
+  }
+});
+
+$("#ele_layer_button").click(function() {
+  //event.preventDefault();
+  if(mymap.hasLayer(eleHotlineLayerG)) {
+      //$(this).removeClass('selected');
+      $("#ele_layer_button").attr('class', 'btn btn-outline-warning');
+      mymap.removeLayer(eleHotlineLayerG);
+      mymap.addLayer(gpx);
+      mymap.addLayer(markersLayerG);
+  } else {
+      mymap.addLayer(eleHotlineLayerG);   
+      mymap.addLayer(markersLayerG);
+      if(mymap.hasLayer(gpx)){mymap.removeLayer(gpx);}
+      if(mymap.hasLayer(hrHotlineLayerG)){mymap.removeLayer(hrHotlineLayerG);}
+      if(mymap.hasLayer(cadHotlineLayerG)){mymap.removeLayer(cadHotlineLayerG);}
+      if(mymap.hasLayer(tempHotlineLayerG)){mymap.removeLayer(tempHotlineLayerG);}
+      
+      //$(this).addClass('selected');
+      $("#hr_layer_button").attr('class', 'btn btn-outline-danger');
+      $("#ele_layer_button").attr('class', 'btn btn-warning');
+      $("#temp_layer_button").attr('class', 'btn btn-outline-info');
+      $("#cad_layer_button").attr('class', 'btn btn-outline-success');
+  }
+});
+
+$("#cad_layer_button").click(function() {
+  //event.preventDefault();
+  if(mymap.hasLayer(cadHotlineLayerG)) {
+      //$(this).removeClass('selected');
+      $("#cad_layer_button").attr('class', 'btn btn-outline-success');
+      mymap.removeLayer(cadHotlineLayerG);
+      mymap.addLayer(gpx);
+      mymap.addLayer(markersLayerG);
+  } else {
+      mymap.addLayer(cadHotlineLayerG);
+      mymap.addLayer(markersLayerG);
+      if(mymap.hasLayer(gpx)){mymap.removeLayer(gpx);}
+      if(mymap.hasLayer(eleHotlineLayerG)){mymap.removeLayer(eleHotlineLayerG);}
+      if(mymap.hasLayer(hrHotlineLayerG)){mymap.removeLayer(hrHotlineLayerG);}
+      if(mymap.hasLayer(tempHotlineLayerG)){mymap.removeLayer(tempHotlineLayerG);}
+
+      //$(this).addClass('selected');
+      $("#hr_layer_button").attr('class', 'btn btn-outline-danger');
+      $("#ele_layer_button").attr('class', 'btn btn-outline-warning');
+      $("#temp_layer_button").attr('class', 'btn btn-outline-info');
+      $("#cad_layer_button").attr('class', 'btn btn-success');
+  }
+});
+
+$("#temp_layer_button").click(function() {
+  //event.preventDefault();
+  if(mymap.hasLayer(tempHotlineLayerG)) {
+      //$(this).removeClass('selected');
+      $("#temp_layer_button").attr('class', 'btn btn-outline-info');
+      mymap.removeLayer(tempHotlineLayerG);
+      mymap.addLayer(gpx);
+      mymap.addLayer(markersLayerG);
+  } else {
+      mymap.addLayer(tempHotlineLayerG);
+      mymap.addLayer(markersLayerG);
+      if(mymap.hasLayer(gpx)){mymap.removeLayer(gpx);}
+      if(mymap.hasLayer(eleHotlineLayerG)){mymap.removeLayer(eleHotlineLayerG);}
+      if(mymap.hasLayer(cadHotlineLayerG)){mymap.removeLayer(cadHotlineLayerG);}
+      if(mymap.hasLayer(hrHotlineLayerG)){mymap.removeLayer(hrHotlineLayerG);}
+
+      //$(this).addClass('selected');
+      $("#hr_layer_button").attr('class', 'btn btn-outline-danger');
+      $("#ele_layer_button").attr('class', 'btn btn-outline-warning');
+      $("#temp_layer_button").attr('class', 'btn btn-info');
+      $("#cad_layer_button").attr('class', 'btn btn-outline-success');
+  }
+});
 
 //$.when(display_gpx(document.getElementById('demo'))).then(draw_hotline_all());
 //display_gpx(document.getElementById('demo'));
